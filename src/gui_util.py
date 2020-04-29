87523b30679e7d13181cea50b7987fe8cb2c464e
Mikhail Hyde <mhyde64@my.uri.edu>
2020-04-29 11:29:32 -0400

    Cleaning before UI OverHaul

---
+++ src/gui_util.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+'''
+WPRI-IF Robot Control Software
+Copyright (C) 2020  Mikhail Hyde
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import tkinter as tk
+
+def manPrime(entry, poseSuper):
+    '''
+    Allows user to manually set the X-Axis Pose
+    '''
+    temp = int(entry.get())
+    print('Moving to %d mm' % temp)
+    poseSuper.positionxAxis(temp)
+
+
+def generateRadioButtons(inProfile, frame, var):
+    '''
+    Generates radio buttons for each channel in the
+    given profile
+    '''
+    for key in inProfile.keys():
+        but = tk.Radiobutton(frame, variable=var, value=key)
+        but.grid(row=1, column=key)
+        l = tk.Label(frame, text=inProfile[key][1])
+        l.grid(row=2, column=key)
+
+def startupMsg():
+    '''
+    Displays important information in terminal
+    during program startup.
+    '''
+    print(
+        '''
+Welcome to the Index Finger Control System
+
+This system is designed to remotely activate the channels
+of a rack unit. Simply select the channel that you would
+like to activate, press the Prime button to position the
+switch, then the Take button to activate the switching
+mechanism. The GUI is designed so that only one command
+can be executed at once. For example, you cannot Take while
+the system in Priming.
+
+#############################
+##       X-AXIS LIMIT      ##
+#############################
+
+      5 <= xPose <= 495
+
+Please do not exceed this limit when manually setting
+the X Position.
+
+#############################
+##          BUTTONS        ##
+#############################
+
+Radio Buttons (Top Row) - Selects Channel
+Prime - Positions Switching Mechanism as currently selected Channel
+Take - Commands the switcher to extend and retract, activating the button
+       at the current location.
+Manual Prime - Sets the X-Position to the value inside the entry box
+Calibrate - Causes the Switching Mechanism to move in a negative direction
+            along the X-Axis until the origin is met. It will then move one
+            rotation positively from the origin.
+
+#############################
+##           HELP          ##
+#############################
+
+This program will start every time a terminal is opened. Please close the GUI
+to access the terminal. To disable automatic startup, delete:
+
+    python3 ~/wpri-if/ui_app.py
+
+from the last line of ~/.bashrc.
+
+
+SYSTEM IS CALIBRATING! UI WILL OPEN ONCE FINISHED!
+    '''
+    )
--- ui_app.py
+++ ui_app.py
@@ -22,87 +22,24 @@ import tkinter as tk
 from tkinter import font as tkFont
 
 from src.position_supervisor import positionSupervisor
+from src.gui_util import generateRadioButtons, manPrime, startupMsg
 from profiles.wpri_server import profile
 
 poseSuper = positionSupervisor([5, 6, 13, 19, 26], 'EGTH', 22, 4)
-
-
-def manPrime():
-    '''
-    Allows user to manually set the X-Axis Pose
-    '''
-    temp = int(e1.get())
-    print('Moving to %d mm' % temp)
-    poseSuper.positionxAxis(temp)
-
-
-def generateRadioButtons(inProfile):
-    '''
-    Generates radio buttons for each channel in the
-    given profile
-    '''
-    for key in inProfile.keys():
-        but = tk.Radiobutton(radioFrame, variable=v, value=key)
-        but.grid(row=1, column=key)
-        l = tk.Label(radioFrame, text=inProfile[key][1])
-        l.grid(row=2, column=key)
-
 
 initChoice = input('Would you like to start the IF_Controller UI? [Y/n]   ')
 
 if initChoice.lower() == 'y':
 
-    print(
-        '''
-Welcome to the Index Finger Control System
+    startupMsg()
 
-This system is designed to remotely activate the channels
-of a rack unit. Simply select the channel that you would
-like to activate, press the Prime button to position the
-switch, then the Take button to activate the switching
-mechanism. The GUI is designed so that only one command
-can be executed at once. For example, you cannot Take while
-the system in Priming.
-
-####    X-AXIS LIMIT    ####
-
-      5 <= xPose <= 495
-
-Please do not exceed this limit when manually setting
-the X Position.
-
-#############################
-##                         ##
-##          BUTTONS        ##
-##                         ##
-#############################
-
-Radio Buttons (Top Row) - Selects Channel
-Prime - Positions Switching Mechanism as currently selected Channel
-Take - Commands the switcher to extend and retract, activating the button
-       at the current location.
-Manual Prime - Sets the X-Position to the value inside the entry box
-Calibrate - Causes the Switching Mechanism to move in a negative direction
-            along the X-Axis until the origin is met. It will then move one
-            rotation positively from the origin.
-
-This program will start every time a terminal is opened. Please close the GUI
-to access the terminal. To disable automatic startup, delete:
-
-    python3 ~/wpri-if/ui_app.py
-
-from the last line of ~/.bashrc.
-
-
-SYSTEM IS CALIBRATING! UI WILL OPEN ONCE FINISHED!
-'''
-    )
-
+    # Main Window Config
     root = tk.Tk(className='WPRI-IF ::: Server Controller')
     root.geometry('900x350')
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
 
+    # Define frames
     radioFrame = tk.Frame(root)
     radioFrame.grid(row=0, column=0, rowspan=2, sticky='')
 
@@ -115,11 +52,14 @@ SYSTEM IS CALIBRATING! UI WILL OPEN ONCE FINISHED!
     eFrame = tk.Frame(root)
     eFrame.grid(row=6, column=0, columnspan=4)
 
+    # Defining font
     helv36 = tkFont.Font(family='Helvetica', size=16, weight=tkFont.BOLD)
 
+    # Tkinter Variables
     v = tk.IntVar(value=1)
 
-    generateRadioButtons(profile)
+    # Generate Control Buttons
+    generateRadioButtons(radioFrame, profile, v)
 
     b1 = tk.Button(ptButtonFrame, text='Prime', height=2, width=15,
                    font=helv36, borderwidth=2, relief=tk.SOLID,
@@ -135,6 +75,7 @@ SYSTEM IS CALIBRATING! UI WILL OPEN ONCE FINISHED!
                    command=lambda: poseSuper.calibrate(profile[1][0]))
     b3.grid(row=3, column=5, columnspan=3)
 
+    # Generate Manual Priming Controls
     e1 = tk.Entry(eFrame)
     e1.grid(row=3, column=2, columnspan=2)
     but3 = tk.Button(eFrame, text='Manual Prime', height=1, width=10, borderwidth=2,
@@ -143,6 +84,8 @@ SYSTEM IS CALIBRATING! UI WILL OPEN ONCE FINISHED!
     l1 = tk.Label(eFrame, text='X-Axis Jog')
     l1.grid(row=3, column=0)
 
+    # Calibrate
     poseSuper.calibrate(profile[1][0])
 
+    # Start UI
     root.mainloop()
